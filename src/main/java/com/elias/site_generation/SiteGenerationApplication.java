849df844abe1bafaacde03ba32860514b2dc78ea
HaaaamBurger <gemasclashes@gmail.com>
2026-08-14 12:00:20 +0300

    init

---
+++ src/main/java/com/elias/site_generation/SiteGenerationApplication.java
@@ -0,0 +1,13 @@
+package com.elias.site_generation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteGenerationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteGenerationApplication.class, args);
+	}
+
+}
