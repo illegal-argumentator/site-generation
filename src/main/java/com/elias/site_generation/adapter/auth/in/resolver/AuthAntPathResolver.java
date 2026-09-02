d11d8751c96241ee5e9122220d8d9f6b0dbd410a
HaaaamBurger <gemasclashes@gmail.com>
2026-09-02 10:49:57 +0300

    unauthenticated paths

---
+++ src/main/java/com/elias/site_generation/adapter/auth/in/resolver/AuthAntPathResolver.java
@@ -0,0 +1,24 @@
+package com.elias.site_generation.adapter.auth.in.resolver;
+
+import com.elias.site_generation.shared.props.PathProps;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
+
+import java.util.Arrays;
+
+@Component
+@RequiredArgsConstructor
+public class AuthAntPathResolver implements AntPathResolver {
+
+    private static final AntPathMatcher antPathMatcher = new AntPathMatcher();
+
+    private final PathProps props;
+
+    @Override
+    public boolean isPermittedPath(String path) {
+        return Arrays.stream(props.allPaths())
+                .anyMatch(pr -> antPathMatcher.match(pr, path));
+    }
+
+}
