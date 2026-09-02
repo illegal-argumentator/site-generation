0dd559839ae16e4c8d771e85ce3ed538c64b7d96
HaaaamBurger <gemasclashes@gmail.com>
2026-09-02 15:35:09 +0300

    cors enabled

--- src/main/java/com/elias/site_generation/adapter/auth/out/config/SecurityConfig.java
+++ src/main/java/com/elias/site_generation/adapter/auth/out/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.elias.site_generation.shared.props.PathProps;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -32,7 +33,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .cors(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .anonymous(AbstractHttpConfigurer::disable)
                 .exceptionHandling(configurer -> configurer
                         .authenticationEntryPoint(authenticationEntryPoint)
