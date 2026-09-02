d9444c180c83593266e51394ed9183418f49f49e
HaaaamBurger <gemasclashes@gmail.com>
2026-09-02 12:24:41 +0300

    exception handling, configs

--- src/main/java/com/elias/site_generation/adapter/auth/in/resolver/AuthAntPathResolver.java
+++ src/main/java/com/elias/site_generation/adapter/auth/in/resolver/AuthAntPathResolver.java
@@ -17,8 +17,6 @@ public class AuthAntPathResolver implements AntPathResolver {
 
     @Override
     public boolean isPermittedPath(String path) {
-        System.out.println(props.allPaths());
-        System.out.printf(path);
         return Arrays.stream(props.allPaths())
                 .anyMatch(pr -> antPathMatcher.match(pr, path));
     }
