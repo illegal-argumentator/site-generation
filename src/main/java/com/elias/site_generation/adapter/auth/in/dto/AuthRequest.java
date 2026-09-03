75fa9361be1cdd6b548e12f846abb306919e2432
HaaaamBurger <gemasclashes@gmail.com>
2026-09-03 10:49:46 +0300

    removed owner from site entity

--- src/main/java/com/elias/site_generation/adapter/auth/in/dto/AuthRequest.java
+++ src/main/java/com/elias/site_generation/adapter/auth/in/dto/AuthRequest.java
@@ -1,15 +1,18 @@
 package com.elias.site_generation.adapter.auth.in.dto;
 
 import com.elias.site_generation.shared.user.UserRegexPatterns;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 
 public record AuthRequest(
 
+        @Schema(example = "admin@gmail.com")
         @NotNull(message = "Email is required.")
         @Pattern(regexp = UserRegexPatterns.EMAIL, message = "Invalid email. Example format: example@domain.com")
         String email,
 
+        @Schema(example = "Qwerty1234@")
         @NotNull(message = "Password is required.")
         @Pattern(regexp = UserRegexPatterns.PASSWORD, message = "Invalid password. Should include minimum 8 characters, 1 uppercase character, 1 lowercase character, 1 special symbol.")
         String password
