d6339bca82d721ad62fc3e702cf2068ebad4d281
HaaaamBurger <gemasclashes@gmail.com>
2026-09-09 14:20:13 +0300

    uploading images via index.php

--- src/main/java/com/elias/site_generation/adapter/theme/out/generation/component/template/TemplateComponentsApplier.java
+++ src/main/java/com/elias/site_generation/adapter/theme/out/generation/component/template/TemplateComponentsApplier.java
@@ -46,7 +46,7 @@ final class TemplateComponentsApplier {
         Iterator<String> iterator = images.iterator();
         for (Element imageEl : imageEls) {
             String srcVal = imageEl.attr(SOURCE_ELEMENT);
-            imageEl.attr(SOURCE_ELEMENT, srcVal + props.getAssetsLocalPath().concat(iterator.next()));
+            imageEl.attr(SOURCE_ELEMENT, srcVal + props.getAssetsPath().concat(iterator.next()));
         }
     }
 
