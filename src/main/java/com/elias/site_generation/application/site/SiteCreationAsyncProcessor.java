e763bf79b1a58f4ae13f8e81b055dd4a2319e980
HaaaamBurger <gemasclashes@gmail.com>
2026-09-23 11:47:56 +0200

    creation status fix v2

--- src/main/java/com/elias/site_generation/application/site/SiteCreationAsyncProcessor.java
+++ src/main/java/com/elias/site_generation/application/site/SiteCreationAsyncProcessor.java
@@ -8,6 +8,7 @@ import com.elias.site_generation.domain.site.type.DeployStatus;
 import com.elias.site_generation.domain.theme.TemplateType;
 import com.elias.site_generation.domain.theme.Theme;
 import com.elias.site_generation.domain.theme.event.ThemeDeployEvent;
+import com.elias.site_generation.domain.theme.event.ThemePostDeployEvent;
 import com.elias.site_generation.domain.user.User;
 import com.elias.site_generation.port.site.DbGenerationPort;
 import com.elias.site_generation.port.site.SiteCommandPort;
@@ -79,7 +80,7 @@ class SiteCreationAsyncProcessor {
 
     private void publishDeploy(Site site) {
         siteCommandPort.update(site.getId(), Site.builder().deployStatus(DeployStatus.IN_PROGRESS).build());
-        publisher.publishEvent(new ThemeDeployEvent(site));
+        publisher.publishEvent(site.getDeployStatus() == DeployStatus.THEME_REMOVE_FAILED ? new ThemePostDeployEvent(site) : new ThemeDeployEvent(site));
     }
 
 }
